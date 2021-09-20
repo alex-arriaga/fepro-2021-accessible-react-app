7b804a9d25db4896c301670d8c5be07b72107c52
Alex Arriaga <alex.arriaga@webtraining.world>
2021-09-19 17:19:23 -0700

    Arreglar formulario

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -5,25 +5,34 @@ function Register() {
     return (
         <Fragment>
             <h2 className="mt-3 mb-4">Registro</h2>
-            <form action="">
+            <form action="" className="form-register">
                 <div className="mb-3">
-                    <input type="name" className="form-control"
-                           placeholder="Nombre"/>
+                    <label htmlFor="fullName" className="form-label">Nombre completo</label>
+                    <input type="name" className="form-control" name="fullName" id="fullName"
+                           required
+                           placeholder="Ingrese nombre completo, por ejemplo: Sofía Mendoza Uribe"/>
                 </div>
                 <div className="mb-3">
-                    <input type="email" className="form-control"
-                           placeholder="Correo electrónico"/>
+                    <label htmlFor="email" className="form-label">Correo electrónico</label>
+                    <input type="email" className="form-control" name="email" id="email"
+                           required
+                           placeholder="Ingrese correo electrónico, por ejemplo: sofia@gmail.com"/>
                 </div>
                 <div className="mb-3">
-                    <input type="password" className="form-control"
-                           placeholder="Contraseña"/>
+                    <label htmlFor="password" className="form-label">Contraseña</label>
+                    <input type="password" className="form-control" name="password" id="password"
+                           required pattern="[0-9]+" minLength="4"
+                           placeholder="Ingrese una contraseña"/>
+                    <p className="text-secondary mt-2">La contraseña debe contener al menos 4 dígitos</p>
                 </div>
                 <div className="mb-3">
-                    <input type="password" className="form-control"
-                           placeholder="Confirmar contraseña"/>
+                    <label htmlFor="confirmPassword" className="form-label">Confirmar contraseña</label>
+                    <input type="password" className="form-control" name="confirmPassword" id="confirmPassword"
+                           required pattern="[0-9]+" minLength="4"
+                           placeholder="Confirme su contraseña"/>
                 </div>
                 <div className="mb-3">
-                    <button className="btn btn-secondary">Enviar</button>
+                    <button className="btn btn-secondary" type="submit">Enviar</button>
                 </div>
             </form>
         </Fragment>
